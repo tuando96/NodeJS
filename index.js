a897ff3abec1f0a1009dcdacdea0631a4a78ea8d
Tuan <tuando961@gmail.com>
2021-07-20 19:44:08 +0700

    demo nodejs

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express(); //tao 1 ung dung tu express module
-var port = 5000;
+var port = process.env.PORT || 5000;
 app.listen(port,function (){
     console.log("Server is running....");
 });
